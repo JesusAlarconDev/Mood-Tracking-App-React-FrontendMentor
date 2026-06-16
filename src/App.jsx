39f26777ac1bbc4e3f6b018b6c8801922c0d5762
JesusAlarconDev <alarconmjesusm@gmail.com>
2026-06-15 21:26:30 -0400

    Feat: Add routing, protected routes, and backend base

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import './App.css'
+import { BrowserRouter, Routes, Route, useLocation, Navigate } from 'react-router-dom'
 import { Home } from './components/Home'
+import Login from './components/Auth/Login'
+import Register from './components/Auth/Register'
+import { AuthRoute } from './context/AuthContext'
 
 function App() {
+  const location = useLocation();
+  const isAuthRoute = location.pathname === '/login' || location.pathname === '/signup';
 
+  if(isAuthRoute) {
+    return (
+      <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Register />} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    )
+  }
   return (
     <>
-      <Home />
+      <Routes>
+        <Route path="/" element={<AuthRoute><Home /></AuthRoute>} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Redirige cualquier ruta que empiece con '/api'
+      '/api': {
+        target: 'http://localhost:3000', // La URL de tu backend
+        changeOrigin: true, // Importante para la virtualización
+        secure: false, // Puedes dejarlo en false en desarrollo local
+      },
+    },
+  },
 })
